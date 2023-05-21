1e184d178a945b694f87125c543974f8b413bfba
Ruslan <Giovingo92@gmail.com>
2023-05-21 22:34:29 +0300

    rewriting on hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 
@@ -6,38 +6,79 @@ import { Backdrop, ModalContent } from './Modal,styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export const Modal = ({ onClose, src, alt }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
-  handleKeyDown = e => {
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClose = e => {
+  const handleBackdropClose = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <Backdrop onClick={this.handleBackdropClose}>
-        <ModalContent>
-          <img src={this.props.src} alt={this.props.alt} />
-        </ModalContent>
-      </Backdrop>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <Backdrop onClick={handleBackdropClose}>
+      <ModalContent>
+        <img src={src} alt={alt} />
+      </ModalContent>
+    </Backdrop>,
+    modalRoot
+  );
+};
+
+Modal.propTypes = {
+  // handleBackdropClose => ???????????
+  handleBackdropClose: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// export class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropClose = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <Backdrop onClick={this.handleBackdropClose}>
+//         <ModalContent>
+//           <img src={this.props.src} alt={this.props.alt} />
+//         </ModalContent>
+//       </Backdrop>,
+//       modalRoot
+//     );
+//   }
+// }
 
 Modal.propTypes = {
   handleBackdropClose: PropTypes.func.isRequired,
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,9 +1,8 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Form, Header, Span, Input } from './Searchbar.styled';
 
-export class SearchBar extends Component {
-  handleSubmit = e => {
+export const SearchBar = ({ onSubmit }) => {
+  const handleSubmit = e => {
     e.preventDefault();
 
     const query = e.target.elements.search.value.toLocaleLowerCase().trim();
@@ -12,23 +11,49 @@ export class SearchBar extends Component {
       return;
     }
 
-    this.props.onSubmit(query);
+    onSubmit(query);
   };
 
-  render() {
-    return (
-      <Header>
-        <Form onSubmit={this.handleSubmit}>
-          <Button type="submit">
-            <Span>Search</Span>
-          </Button>
+  return (
+    <Header>
+      <Form onSubmit={handleSubmit}>
+        <Button type="submit">
+          <Span>Search</Span>
+        </Button>
 
-          <Input type="text" name="search" />
-        </Form>
-      </Header>
-    );
-  }
-}
+        <Input type="text" name="search" />
+      </Form>
+    </Header>
+  );
+};
+
+// export class SearchBar extends Component {
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     const query = e.target.elements.search.value.toLocaleLowerCase().trim();
+
+//     if (!query) {
+//       return;
+//     }
+
+//     this.props.onSubmit(query);
+//   };
+
+//   render() {
+//     return (
+//       <Header>
+//         <Form onSubmit={this.handleSubmit}>
+//           <Button type="submit">
+//             <Span>Search</Span>
+//           </Button>
+
+//           <Input type="text" name="search" />
+//         </Form>
+//       </Header>
+//     );
+//   }
+// }
 
 SearchBar.protoTypes = {
   onSubmit: PropTypes.func.isRequired,
