acb1b1408822b58d9d69bdf95ff1db07a9861af0
Ruslan <Giovingo92@gmail.com>
2023-05-22 00:15:12 +0300

    rewritten to hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 import * as ImageService from '../../service/api';
 
@@ -9,104 +9,182 @@ import { Loader } from 'components/Loader/Loader';
 import { LoadMore } from 'components/Button/Button';
 import { Modal } from '../Modal/Modal';
 
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    images: [],
-    isLoading: false,
-    error: '',
-    totalImages: 0,
-    showModal: false,
-    largeImageURL: '',
-    tags: '',
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [totalImages, settoTalImages] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [tags, setTags] = useState('');
 
-  async componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-
-    console.log(this.state);
-
-    if (prevState.query !== query || prevState.page !== page) {
+  useEffect(() => {
+    if (!query) return;
+    const fetchData = async () => {
       try {
-        this.setState({ isLoading: true, error: '' });
-        const data = await ImageService.getImages(query, page);
-
-        const images = data.hits.map(
-          ({ id, tags, webformatURL, largeImageURL }) => {
-            return {
-              id,
-              tags,
-              webformatURL,
-              largeImageURL,
-            };
-          }
-        );
-
-        this.setState(prevState => {
-          return {
-            images: [...prevState.images, ...images],
-            totalImages: data.totalHits,
-          };
-        });
+        setIsLoading(true);
+        const { hits, totalHits } = await ImageService.getImages(query, page);
+        setImages(prevImages => [...prevImages, ...hits]);
+        settoTalImages(totalHits);
       } catch (error) {
-        this.setState({ error: 'Something went wrong' });
+        setError({ error: 'Something went wrong' });
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
-    }
-  }
+    };
 
-  addPage = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
+    fetchData();
+  }, [query, page]);
+
+  const addPage = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  getQuery = query => {
-    if (query === this.state.query) {
+  const getQuery = query => {
+    if (!query) {
       alert('Change search');
       return;
     }
-    this.setState({ query, page: 1, images: [], totalImages: 0 });
+    setQuery(query);
+    setPage(1);
+    setImages([]);
+    settoTalImages(0);
   };
 
-  openModal = (src, alt) => {
-    this.setState({ largeImageURL: src, tabs: alt, showModal: true });
+  const openModal = (src, alt) => {
+    setLargeImageURL(src);
+    setTags(alt);
+    setShowModal(true);
   };
 
-  closeMoadal = () => {
-    this.setState({ largeImageURL: null, tabs: null, showModal: false });
+  const closeMoadal = () => {
+    setLargeImageURL(null);
+    setTags(null);
+    setShowModal(false);
   };
 
-  render() {
-    const {
-      images,
-      error,
-      isLoading,
-      totalImages,
-      showModal,
-      largeImageURL,
-      tags,
-    } = this.state;
+  const showButton = images.length !== totalImages && !isLoading;
 
-    const showButton = images.length !== totalImages && !isLoading;
+  return (
+    <Container>
+      <Section>
+        <SearchBar onSubmit={getQuery} />
+        {isLoading && <Loader />}
+        {images.length > 0 && (
+          <ImageGallery images={images} openModal={openModal} />
+        )}
+        {error && <p>{error}</p>}
+        {showButton && <LoadMore onClick={addPage} />}
+        {showModal && (
+          <Modal src={largeImageURL} alt={tags} onClose={closeMoadal} />
+        )}
+      </Section>
+    </Container>
+  );
+};
 
-    return (
-      <Container>
-        <Section>
-          <SearchBar onSubmit={this.getQuery} />
-          {isLoading && <Loader />}
-          {images.length > 0 && (
-            <ImageGallery images={images} openModal={this.openModal} />
-          )}
-          {error && <p>{error}</p>}
-          {showButton && <LoadMore onClick={this.addPage} />}
-          {showModal && (
-            <Modal src={largeImageURL} alt={tags} onClose={this.closeMoadal} />
-          )}
-        </Section>
-      </Container>
-    );
-  }
-}
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// export class App extends Component {
+//   state = {
+//     query: '',
+//     page: 1,
+//     images: [],
+//     isLoading: false,
+//     error: '',
+//     totalImages: 0,
+//     showModal: false,
+//     largeImageURL: '',
+//     tags: '',
+//   };
+
+//   async componentDidUpdate(_, prevState) {
+//     const { query, page } = this.state;
+
+//     console.log(this.state);
+
+//     if (prevState.query !== query || prevState.page !== page) {
+//       try {
+//         this.setState({ isLoading: true, error: '' });
+//         const data = await ImageService.getImages(query, page);
+
+//         const images = data.hits.map(
+//           ({ id, tags, webformatURL, largeImageURL }) => {
+//             return {
+//               id,
+//               tags,
+//               webformatURL,
+//               largeImageURL,
+//             };
+//           }
+//         );
+
+//         this.setState(prevState => {
+//           return {
+//             images: [...prevState.images, ...images],
+//             totalImages: data.totalHits,
+//           };
+//         });
+//       } catch (error) {
+//         this.setState({ error: 'Something went wrong' });
+//       } finally {
+//         this.setState({ isLoading: false });
+//       }
+//     }
+//   }
+
+//   addPage = () => {
+//     this.setState(prevState => {
+//       return { page: prevState.page + 1 };
+//     });
+//   };
+
+//   getQuery = query => {
+//     if (query === this.state.query) {
+//       alert('Change search');
+//       return;
+//     }
+//     this.setState({ query, page: 1, images: [], totalImages: 0 });
+//   };
+
+//   openModal = (src, alt) => {
+//     this.setState({ largeImageURL: src, tabs: alt, showModal: true });
+//   };
+
+//   closeMoadal = () => {
+//     this.setState({ largeImageURL: null, tabs: null, showModal: false });
+//   };
+
+//   render() {
+//     const {
+//       images,
+//       error,
+//       isLoading,
+//       totalImages,
+//       showModal,
+//       largeImageURL,
+//       tags,
+//     } = this.state;
+
+//     const showButton = images.length !== totalImages && !isLoading;
+
+//     return (
+//       <Container>
+//         <Section>
+//           <SearchBar onSubmit={this.getQuery} />
+//           {isLoading && <Loader />}
+//           {images.length > 0 && (
+//             <ImageGallery images={images} openModal={this.openModal} />
+//           )}
+//           {error && <p>{error}</p>}
+//           {showButton && <LoadMore onClick={this.addPage} />}
+//           {showModal && (
+//             <Modal src={largeImageURL} alt={tags} onClose={this.closeMoadal} />
+//           )}
+//         </Section>
+//       </Container>
+//     );
+//   }
+// }
